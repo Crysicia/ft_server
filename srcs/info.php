f4591a67a8ec239508f871333fc80b818e65c7ae
Crysicia <7459685+Crysicia@users.noreply.github.com>
2020-12-13 01:39:23 +0100

    Check php connection to mariadb

--- srcs/info.php
+++ srcs/info.php
@@ -1,3 +1,18 @@
 <?php
-phpinfo();
+$user = "user42";
+$password = "user42";
+$database = "todo";
+$table = "list";
+
+try {
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+  echo "<h2>TODO</h2><ol>";
+  foreach($db->query("SELECT content FROM $table") as $row) {
+    echo "<li>" . $row['content'] . "</li>";
+  }
+  echo "</ol>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
 ?>
